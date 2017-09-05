31f10dadfee7f71546b93c5b87d4770b1024796f
Rafael S. de Souza <rafael.2706@gmail.com>
2017-09-05 16:52:29 -0400

    k

---
+++ R_script/read_3HeDp_data.R
@@ -0,0 +1,55 @@
+# Read 3HeDp 
+require(ggplot2);require(ggthemes);require(dplyr);require(magrittr)
+
+#--Get File names
+path <- "../data/3Hedp/hdp_data-2017"
+files <- list.files(path = path)
+
+# Data of interest
+
+Mol80 <- read.table(paste(path,"hdp_mol80b.dat",sep="/"),header = F) %>%
+         set_colnames(.,c("E","Syst","S","Stat")) %>%
+         mutate(.,Syst= c(0.28,0.55,0.65,0.72,0.67,0.69,0.58,
+                          0.52,0.44,0.4,0.29,0.2,0.15,0.11,
+                          0.09,0.08,0.07,0.06)) %>%
+        select(.,c("E","S","Stat","Syst")) %>%
+        mutate(.,dat="Mol80")
+
+Kra87m <- read.csv(paste(path,"hdp_kra87m.csv",sep="/"),header = T,sep="") %>% 
+          mutate(.,Syst= 0.078) %>% 
+          mutate(.,dat="Kra87m")
+
+hdp_kra87b <- read.csv(paste(path,"hdp_kra87b.csv",sep="/"),header = T,sep="")  %>% 
+              mutate(.,Syst= 0.06) %>%  
+              mutate(.,dat="hdp_kra87b")
+
+zhi77b <- read.table(paste(path,"hdp_zhi77b.dat",sep="/"),header = F) %>%
+          set_colnames(c("E","E_rr","S","Stat"))  %>%  
+          mutate(.,Syst=0.034) %>%  
+          mutate(.,dat="zhi77b") %>% 
+          select(.,c(-E_rr))
+
+gei99b <- read.table(paste(path,"hdp_gei99b.dat",sep="/"),header = F) %>%
+  set_colnames(.,c("E","Syst","S","Stat")) %>%
+  mutate(.,Syst= 0.043) %>%
+  select(.,c("E","S","Stat","Syst")) %>%
+  mutate(.,dat="gei99b")
+
+
+gei99d <- read.table(paste(path,"hdp_gei99d.dat",sep="/"),header = F) %>%
+  set_colnames(.,c("E","Syst","S","Stat")) %>%
+  mutate(.,Syst= 0.043) %>%
+  select(.,c("E","S","Stat","Syst")) %>%
+  mutate(.,dat="gei99d")
+
+
+ensamble <- rbind(Mol80,Kra87m,hdp_kra87b,zhi77b,gei99b,gei99d)
+
+write.csv(ensamble,"ensamble.csv",row.names = F)
+
+ggplot(ensamble,aes(x=E,y=S,color=dat,shape=dat))+geom_point(size=2.85)+
+  theme_bw()+scale_x_log10()+scale_color_hc() +
+  coord_cartesian(xlim=c(5e-3,1))
+
+
+
