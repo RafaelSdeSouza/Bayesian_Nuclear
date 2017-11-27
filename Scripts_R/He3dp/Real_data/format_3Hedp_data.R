1ff7d4007bd81fe942e18e10d88cedc718785b86
Rafael S. de Souza <rafael.2706@gmail.com>
2017-11-27 14:46:58 -0500

    w

--- Scripts_R/He3dp/Real_data/format_3Hedp_data.R
+++ Scripts_R/He3dp/Real_data/format_3Hedp_data.R
@@ -2,10 +2,28 @@
 require(ggplot2);require(ggthemes);require(dplyr);require(magrittr)
 
 #--Get File names
-path <- "../data/3Hedp/hdp_data-2017"
+path <- "/Users/rafaeldesouza/Documents/GitHub/JAGS_UNC/data/3Hedp"
 files <- list.files(path = path)
 
 # Data of interest
+
+
+Ali01 <-  read.table(paste(path,"Ali01.dat",sep="/"),header = F) %>%
+  set_colnames(.,c("E","S","Stat","E2","S2","Stat2")) %>%
+  mutate(.,Syst=0.03) %>%  
+  mutate(.,dat="Ali01") %>% 
+  select(.,c("E","S","Stat","Syst","dat"))
+
+
+
+Cos00 <-  read.table(paste(path,"data_cos00a.dat",sep="/"),header = F) %>%
+  set_colnames(.,c("Ebeam","E","S","Stat","Syst")) %>%
+  mutate(.,Syst=0.055) %>%  
+  mutate(.,dat="Cos00") %>% 
+  select(.,c(-Ebeam))
+
+
+
 
 Lac05 <-  read.table(paste(path,"hdp_thm05b.dat",sep="/"),header = F) %>%
   set_colnames(.,c("E","E_rr","S","Stat")) %>%
@@ -22,35 +40,35 @@ Mol80 <- read.table(paste(path,"hdp_mol80b.dat",sep="/"),header = F) %>%
         select(.,c("E","S","Stat","Syst")) %>%
         mutate(.,dat="Mol80")
 
-Kra87m <- read.csv(paste(path,"hdp_kra87m.csv",sep="/"),header = T,sep="") %>% 
+Kra87 <- read.table(paste(path,"data_krauss.dat",sep="/"),header = F,sep="") %>% 
+          set_colnames(.,c("E","S","Stat")) %>%
           mutate(.,Syst= 0.078) %>% 
-          mutate(.,dat="Kra87m")
+          mutate(.,dat="Kra87")
 
-Kra87b <- read.csv(paste(path,"hdp_kra87b.csv",sep="/"),header = T,sep="")  %>% 
-              mutate(.,Syst= 0.078) %>%  
-              mutate(.,dat="Kra87b")
+#Kra87b <- read.csv(paste(path,"hdp_kra87b.csv",sep="/"),header = T,sep="")  %>% 
+#             mutate(.,Syst= 0.078) %>%  
+#             mutate(.,dat="Kra87b")
 
-zhi77b <- read.table(paste(path,"hdp_zhi77b.dat",sep="/"),header = F) %>%
-          set_colnames(c("E","E_rr","S","Stat"))  %>%  
+zhi77b <- read.table(paste(path,"data_zhichang.dat",sep="/"),header = F) %>%
+          set_colnames(c("E","S","Stat"))  %>%  
           mutate(.,Syst=0.034) %>%  
-          mutate(.,dat="zhi77b") %>% 
-          select(.,c(-E_rr))
+          mutate(.,dat="Zhi77") 
 
-gei99b <- read.table(paste(path,"hdp_gei99b.dat",sep="/"),header = F) %>%
-  set_colnames(.,c("E","Syst","S","Stat")) %>%
+#gei99b <- read.table(paste(path,"hdp_gei99b.dat",sep="/"),header = F) %>%
+#  set_colnames(.,c("E","Syst","S","Stat")) %>%
+#  mutate(.,Syst= 0.043) %>%
+ # select(.,c("E","S","Stat","Syst")) %>%
+#  mutate(.,dat="gei99")
+
+
+gei99 <- read.table(paste(path,"data_geist.dat",sep="/"),header = F) %>%
+  set_colnames(.,c("E","S","Stat")) %>%
   mutate(.,Syst= 0.043) %>%
   select(.,c("E","S","Stat","Syst")) %>%
-  mutate(.,dat="gei99b")
+  mutate(.,dat="Gei99")
 
 
-gei99d <- read.table(paste(path,"hdp_gei99d.dat",sep="/"),header = F) %>%
-  set_colnames(.,c("E","Syst","S","Stat")) %>%
-  mutate(.,Syst= 0.043) %>%
-  select(.,c("E","S","Stat","Syst")) %>%
-  mutate(.,dat="gei99d")
-
-
-ensamble <- rbind(Lac05,Mol80,Kra87m,Kra87b,zhi77b,gei99b,gei99d)
+ensamble <- rbind(Ali01,Cos00,Lac05,Mol80,Kra87,zhi77b,gei99 )
 
 write.csv(ensamble,"ensamble.csv",row.names = F)
 
@@ -60,4 +78,22 @@ ggplot(ensamble,aes(x=E,y=S,color=dat))+geom_point(size=2.85)+
   color = "blue", size=1.5)
 
 
+pdf("plot/3Hedp_data.pdf",height = 7,width = 8)
+ggplot(ensamble,aes(x=E,y=S,color=dat,group=dat,shape=dat))+
+  geom_point(size=2)+
+  geom_errorbar(show.legend = FALSE,aes(x=E,y=S,ymin=S-Stat,ymax=S+Stat,group=dat,color=dat),width=0.025)+
+  scale_colour_stata(name="Dataset")+
+  scale_shape_stata(name="Dataset")+
+  theme_wsj() + xlab("Energy (MeV)") + ylab("S-Factor (MeV b)") + scale_x_log10()  +
+  theme(legend.position = "top",
+        legend.background = element_rect(colour = "white", fill = "white"),
+        plot.background = element_rect(colour = "white", fill = "white"),
+        panel.background = element_rect(colour = "white", fill = "white"),
+        legend.key = element_rect(colour = "white", fill = "white"),
+        axis.title = element_text(color="#666666", face="bold", size=15),
+        axis.text  = element_text(size=12),
+        strip.text = element_text(size=10),
+        strip.background = element_rect("gray85")) +
+  ggtitle(expression(paste(NULL^"3","He(d,p)",NULL^"4","He")))
+dev.off()
 
