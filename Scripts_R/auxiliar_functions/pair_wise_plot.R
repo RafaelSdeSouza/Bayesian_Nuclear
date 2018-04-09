2451f458dfd8b071de6cb57f79a65aa1f750eccd
Rafael S. de Souza <rafael.2706@gmail.com>
2018-04-09 18:46:12 -0400

    kk

--- Scripts_R/auxiliar_functions/pair_wise_plot.R
+++ Scripts_R/auxiliar_functions/pair_wise_plot.R
@@ -2,7 +2,8 @@ pair_wise_plot <- function(s){
  
 my_hist <- function(data, mapping, ...) {
     ggplot(data = data, mapping = mapping) +
-     geom_histogram(bins = 10,fill="#bf812d",colour="#1F3552",...) +
+#     geom_histogram(bins = 10,fill="#bf812d",colour="#1F3552",...) +
+    geom_density(fill="#bf812d",colour="#1F3552",...) +
 
       theme_void() + theme( panel.grid.minor=element_blank(),
                             panel.grid.major = element_blank())
@@ -11,18 +12,21 @@ my_hist <- function(data, mapping, ...) {
 my_bin <- function(data, mapping, ..., low = "gray80", high = "gray25") {
   # get the x and y data to use the other code
   ggplot(data = data, mapping = mapping) +
-#    stat_density_2d(aes(fill =..level..), geom="polygon",...) +
+    stat_density_2d(aes(fill =..level..), geom="polygon",...) +
 #    stat_ellipse(type = "norm",level = 0.95,geom = "polygon",fill="#feb24c",...) +
 #    stat_ellipse(type = "norm",level = 0.68,geom = "polygon",fill="#f03b20",...) +
-    geom_bin2d(bins = 50,...) +
-    stat_ellipse(type = "norm",level = 0.997,alpha=0.5,geom = "polygon",fill="#dfc27d",...) +
-    stat_ellipse(type = "norm",level = 0.95,alpha=0.5,geom = "polygon",fill="#bf812d",...) +
-   stat_ellipse(type = "norm",level = 0.68,alpha=0.5,geom = "polygon",fill="#8c510a",...) +
-
+#    geom_bin2d(bins = 50,...) +
+#   stat_ellipse(type = "t",level = 0.997,alpha=0.95,color="#dfc27d",...) +
+    stat_ellipse(type = "t",level = 0.95,alpha=0.95,color="#bf812d",...) +
+   stat_ellipse(type = "t",level = 0.68,alpha=0.95,color="#8c510a",...) +
+    scale_x_continuous(breaks = scales::pretty_breaks(n = 3)) +
+    scale_y_continuous(breaks = scales::pretty_breaks(n = 3)) +
     
     scale_fill_gradient(low = low, high = high) +
     theme_bw() +
-    theme(panel.grid.major = element_blank(),
+    theme( text = element_text(size=10),
+           strip.text = element_text(face="bold", size=20),
+           panel.grid.major = element_blank(),
     panel.grid.minor = element_blank())
 }
 
