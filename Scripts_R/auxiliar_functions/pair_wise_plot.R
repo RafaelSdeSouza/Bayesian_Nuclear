1812c63969c07e3035c73f10f74f35537e8213a1
Rafael S. de Souza <rafael.2706@gmail.com>
2018-06-05 19:10:43 -0400

    d

--- Scripts_R/auxiliar_functions/pair_wise_plot.R
+++ Scripts_R/auxiliar_functions/pair_wise_plot.R
@@ -14,7 +14,7 @@ my_hist <- function(data, mapping, ...) {
                             panel.grid.major = element_blank())
   }
   
-my_bin <- function(data, mapping, ..., low = "gray80", high = "gray15") {
+my_bin <- function(data, mapping, ..., low = "gray90", high = "gray15") {
   # get the x and y data to use the other code
   ggplot(data = data, mapping = mapping,linetype-"dashed",color="red") +
    stat_density_2d(aes(fill =..level..,alpha =..level..),geom="polygon",...) +
@@ -22,7 +22,7 @@ my_bin <- function(data, mapping, ..., low = "gray80", high = "gray15") {
 #    stat_ellipse(type = "norm",level = 0.68,geom = "polygon",fill="#f03b20",...) +
 #    geom_point() +
 #   stat_ellipse(type = "t",level = 0.997,alpha=0.95,color="#dfc27d",...) +
-#    stat_ellipse(type = "t",level = 0.95,alpha=0.95,color="red",...) +
+#   stat_ellipse(type = "t",level = 0.95,alpha=0.95,color="red",...) +
 #   stat_ellipse(type = "t",level = 0.68,alpha=0.95,color="red2",...) +
     scale_x_continuous(breaks = scales::pretty_breaks(n = 4)) +
     scale_y_continuous(breaks = scales::pretty_breaks(n = 4)) +
@@ -30,7 +30,7 @@ my_bin <- function(data, mapping, ..., low = "gray80", high = "gray15") {
     scale_fill_gradient(low = low, high = high) +
     theme_bw() +
     theme( text = element_text(size=60),axis.text = element_text(size=9.5),
-           axis.text.x = element_text(size=8, angle=25),
+           axis.text.x = element_text(size=8, angle=30),
            axis.text.y = element_text(size=8),
            strip.text.x = element_text(colour = 'red',size = 20),
            strip.text.y = element_text(colour = 'red',size = 20),
