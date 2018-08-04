4b8efa4392c1a46073ecfec34a160e318a3c462e
Rafael S. de Souza <rafael.2706@gmail.com>
2018-08-04 12:28:47 -0400

    k

---
+++ Scripts_R/Tdn/Synthetic/reported_errors.R
@@ -0,0 +1,79 @@
+# From: Bayesian Models for Astrophysical Data, Cambridge Univ. Press
+# (c) 2017,  Joseph M. Hilbe, Rafael S. de Souza and Emille E. O. Ishida 
+# 
+# you are kindly asked to include the complete citation if you used this 
+# material in a publication
+
+# Code 4.10 - Normal linear model in R using JAGS and including errors in variables
+
+
+require(R2jags)
+
+
+# Data
+set.seed(1056)                   # set seed to replicate example
+nobs = 1000                      # number of obs in model
+
+sdobsx <- 1.25
+truex <- rnorm(nobs,0,2.5)       # normal variable
+#errx <- rnorm(nobs, 0, sdobsx)
+#obsx <- truex + errx 
+obsx <- truex
+
+beta1 <- -4
+beta2 <- 7
+sdy <- 1.25
+sdobsy <- 2.5 # Variance to simulate reported errors
+
+erry <- rnorm(nobs, 0, sdobsy) # reported errors
+truey <- rnorm(nobs,beta1 + beta2*truex,sdy)
+obsy <- truey + erry
+
+K <- 2
+
+# Level of  mistake in the reported errors
+Lambda <- 0.5
+
+
+model.data <- list(obsy = obsy,
+                   obsx = obsx,
+                   K = K,
+                   errx = errx,
+                   erry = Lambda*erry, # All errors are mistaken by a lambda factor
+                   N = nobs)
+
+NORM_err <-" model{
+# Diffuse normal priors for predictors
+for (i in 1:K) { beta[i] ~ dnorm(0, 1e-3) }
+
+# Uniform prior for standard deviation
+tauy <- pow(sigma, -2)                               # precision
+sigma ~ dunif(0, 100)                                # diffuse prior for standard deviation
+
+
+# Likelihood
+for (i in 1:N){
+obsy[i] ~ dnorm(y[i],pow(erry[i],-2))
+y[i] ~ dnorm(mu[i],tauy)
+mu[i] <- beta[1]+beta[2]*obsx[i] 
+}
+}"
+
+# Initial values
+inits <- function () {
+  list(beta = rnorm(K, 0, 0.01))
+}
+
+# Parameter to display and save
+params <- c("beta", "sigma")
+
+evfit <- jags(data = model.data,
+              inits = inits,
+              parameters = params,
+              model = textConnection(NORM_err),
+              n.chains = 3,
+              n.iter = 5000,
+              n.thin = 1,
+              n.burnin = 2500)
+
+print(evfit,intervals=c(0.025, 0.975), digits=3)
