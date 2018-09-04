6705e4a0da37f99c9ba3bf4f7a62c7f66c066892
Rafael S. de Souza <rafael.2706@gmail.com>
2018-09-04 18:04:11 -0400

    i

--- Scripts_R/He3dp/Real_data/bayes_tools_He3dp_caseII.R
+++ Scripts_R/He3dp/Real_data/bayes_tools_He3dp_caseII.R
@@ -1,6 +1,6 @@
 # preparation: remove all variables from the work space
 rm(list=ls())
-set.seed(42)
+set.seed(27)
 ######################################################################
 # data input
 # format: obsx, obsy, errobsy; the latter are the individual statistical
@@ -63,18 +63,18 @@ likelihood <- function(par){
 }
 
 
-low <- c(0.1,1e-3,rep(1e-4,2), 2,2,1e-4,rep(0.5,7),rep(0,2),obsy - 2*erry)
-up <- c(0.35,1,rep(1.5,2),7,7,5,rep(1.5,7),rep(300,2),obsy + 2*erry)
+low <- c(0.1,1e-3,rep(1e-4,2), 2.5,2.5,1e-4,rep(0.5,7),rep(0,2),obsy - 2*erry)
+up <- c(0.35,1,rep(1.5,2),8,8,5,rep(1.5,7),rep(300,2),obsy + 2*erry)
 
 
 createHedPrior <- function(lower, upper, best = NULL){
 density = function(par){
   d1 = dunif(par[1], 0.1, 0.35,log = TRUE)
-  d2 = dtnorm(par[2], mean = 0, sd = 1,log = TRUE)
+  d2 = dunif(par[1], 0, 1,log = TRUE)
   d3 = dtnorm(par[3], 0, 34.625/par[5]^2, log = TRUE)
   d4 = dtnorm(par[4], 0, 51.94605/par[6]^2, log = TRUE)
 
-  d5 = sum(dunif(par[5:6], 2, 7, log = TRUE))
+  d5 = sum(dunif(par[5:6], 2.5, 8, log = TRUE))
   d6 = dtnorm(par[7], mean = 0, sd = 5, log = TRUE)
   d7 = sum(dlnorm(par[8:14],log(1),log(syst),log = TRUE))
   d8 = sum(dtnorm(par[15:16], mean = 0, sd = 100, log = TRUE))
@@ -86,7 +86,7 @@ sampler = function(){
    c(runif(1, 0.1, 0.35),
    runif(1, 0, 1),
     exp(runif(2, log(1e-3), log(2))),
-    runif(2, 2, 7),
+    runif(2, 2.5, 8),
     runif(1, 0, 5),
     rlnorm(7, log(1), log(syst)), #ynorm
     runif(2, 0, 300),
@@ -117,7 +117,7 @@ names = c("e0","er","gd2","gp2","ad","ap","sigma",to("scale", 7),to("ue", 2),to(
 
 
 
-settings <- list(iterations = 2E6,adaptation = 0.5,thin=10,
+settings <- list(iterations = 5E6,adaptation = 0.5,thin=10,
                  burnin = 1E6, message = T,nrChains = 1)
 
 
@@ -127,8 +127,12 @@ system.time(
 res <- runMCMC(bayesianSetup = setup, settings = settings,sampler = "DREAMzs")
 )
 
+require(MASS)
+write.matrix(ssDat,"He3dp_DREAM.dat")
+
+
 summary(res)
-tracePlot(sampler = res, thin = 1, start = 1E4, whichParameters = c(1,2,3,4,5,6,15,16))
+tracePlot(sampler = res, thin = 1, start = 2.5E4, whichParameters = c(1,2,3,4,5,6,15,16))
 
 
 
@@ -156,8 +160,6 @@ plot_Sfactor_DREAM(ssDat)
 
 
 
-require(MASS)
-write.matrix(ssDat,"He3dp_DREAM.dat")
 
 
 dream_dat <- read.table("He3dp_DREAM.dat",header=T)
